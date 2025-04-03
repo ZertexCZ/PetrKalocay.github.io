1253cc9e1017e5546ee7932a6d3b4088ed374881
Petr Kaločay <petrkalocay@gmail.com>
2025-04-03 18:47:43 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
-// Simple API route to verify Vercel setup
-export default function handler(req, res) {
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).json({ 
     message: 'API is working!',
     path: req.url
